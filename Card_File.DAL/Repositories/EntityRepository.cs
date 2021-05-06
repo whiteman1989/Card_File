99f9493055132d1fa0d6f0dbc1347aa5d1024a5f
AndreyGorban <Whiteman1989@gmail.com>
2021-05-06 11:37:07 +0300

    Finished generic entityRepository

---
+++ Card_File.DAL/Repositories/EntityRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Card_File.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Card_File.DAL.Repositories
+{
+    public class EntityRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public EntityRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Create(T item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var item = _dbSet.Find(id);
+            _dbSet.Remove(item);
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public T Get(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
