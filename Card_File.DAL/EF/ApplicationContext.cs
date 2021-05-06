9519e0f4d97c51557faa6fb5b133ff70c6a819f7
AndreyGorban <Whiteman1989@gmail.com>
2021-05-06 09:44:52 +0300

    Added IRepository inteface and improve ApplicationContext

--- Card_File.DAL/EF/ApplicationContext.cs
+++ Card_File.DAL/EF/ApplicationContext.cs
@@ -9,6 +9,13 @@ namespace Card_File.DAL.EF
 {
     public class ApplicationContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<TextMaterial> TextMaterials { get; set; }
+        public DbSet<MaterialPart> MaterialParts { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
