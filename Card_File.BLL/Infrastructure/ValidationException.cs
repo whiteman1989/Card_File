8d61474ad62c61f3b8fed7a57cc9c5f08cf71039
AndreyGorban <Whiteman1989@gmail.com>
2021-05-15 22:25:55 +0300

    Added ValidationException

---
+++ Card_File.BLL/Infrastructure/ValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_File.BLL.Infrastructure
+{
+    public class ValidationException : Exception
+    {
+        public string Property { get; protected set; }
+        public ValidationException(string message, string prop) : base(message)
+        {
+            Property = prop;
+        }
+    }
+}
