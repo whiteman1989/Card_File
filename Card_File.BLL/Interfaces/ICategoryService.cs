fd27e3310990e66bffc671d4d18bd599603fabc9
AndreyGorban <Whiteman1989@gmail.com>
2021-05-15 22:18:48 +0300

    Added ICategoryService

---
+++ Card_File.BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Card_File.BLL.DTO;
+
+namespace Card_File.BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task CreateCategoryAsunc(CategoryDto category);
+        Task UpdateCategoryAsync(CategoryDto category);
+        Task DeleteCategoryByIdAsync(int id);
+        Task<IEnumerable<CategoryDto>> GetCategoriesAsync();
+        Task<CategoryDto> GetCategoryByIdWithDetailsAsync(int id);
+        Task<IEnumerable<CategoryDto>> GetCategoriesWithDetailsAsync();
+        Task<IEnumerable<CategoryDto>> FindCetegoryesAsync(string term);
+
+    }
+}
