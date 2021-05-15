f57fc57115eee48b80ef06444c25ae68bd9f1762
AndreyGorban <Whiteman1989@gmail.com>
2021-05-15 21:52:32 +0300

    Add migration

--- Card_File.DAL/Migrations/20210515185153_Add IsHide fields to models.Designer.cs
+++ Card_File.DAL/Migrations/20210515185153_Add IsHide fields to models.Designer.cs
@@ -4,14 +4,16 @@ using Card_File.DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Card_File.DAL.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210515185153_Add IsHide fields to models")]
+    partial class AddIsHidefieldstomodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Card_File.DAL/Migrations/20210515185153_Add IsHide fields to models.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Card_File.DAL.Migrations
+{
+    public partial class AddIsHidefieldstomodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHide",
+                table: "TextMaterials",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHide",
+                table: "Comments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHide",
+                table: "TextMaterials");
+
+            migrationBuilder.DropColumn(
+                name: "IsHide",
+                table: "Comments");
+        }
+    }
+}
