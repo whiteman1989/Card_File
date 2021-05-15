5a28c8b6c85ab5246a4ab97b34e2bffee1e098db
AndreyGorban <Whiteman1989@gmail.com>
2021-05-15 23:44:44 +0300

    Added MaterialService

---
+++ Card_File.BLL/Infrastructure/SortingParam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_File.BLL.Infrastructure
+{
+    public enum SortingParam
+    {
+        AddedDate,
+        UpdatedDate,
+        Name,
+        Rating,
+        Comments
+    }
+}
---
+++ Card_File.BLL/Interfaces/IMaterialService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Card_File.BLL.DTO;
+using Card_File.BLL.Infrastructure;
+
+namespace Card_File.BLL.Interfaces
+{
+    public interface IMaterialService
+    {
+        Task CreateMaterialAsync(TextMaterialDto textMaterial);
+        Task UpdateMaterialAsync(TextMaterialDto textMaterial);
+        Task DeleteMeterialByIdAsync(int id);
+        Task HideMaterialByIdAsync(int id);
+        Task UnhideMaterialByIdAsunc(int id);
+        Task<IEnumerable<TextMaterialDto>> GetMaterialsAsync(int skip = 0, int count = 0, bool showHiden = false);
+        Task<IEnumerable<TextMaterialDto>> GetMaterialsSortedWithDetailsAsync(
+            SortingParam sorting = SortingParam.AddedDate,
+            bool reverseOrder = false,
+            int skip = 0,
+            int count = 0,
+            bool showHiden = false);
+        Task<IEnumerable<TextMaterialDto>> GetMaterialsByUserIdWithDetailsAsync(int userId, int skip = 0,
+            int count = 0, bool showHiden = false);
+        Task<TextMaterialDto> GetMaterialByIdWithDetailAsync(int id);
+        Task CreateMaterialPartAsync(MaterialPartDto materialPart);
+        Task UpdateMaterialPartAsync(MaterialPartDto materialPart);
+        Task DeleteMaterialPartByIdAsync(int id);
+        Task<MaterialPartDto> GetMaterialPartByIdAsync(int id);
+    }
+}
