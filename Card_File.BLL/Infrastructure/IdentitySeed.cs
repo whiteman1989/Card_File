4d0c29dacb64f9214ea5502147657dcbf04ad66f
AndreyGorban <Whiteman1989@gmail.com>
2021-05-28 22:33:52 +0300

    Added Identity seeding

---
+++ Card_File.BLL/Infrastructure/IdentitySeed.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Card_File.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+
+namespace Card_File.BLL.Infrastructure
+{
+	public static class IdentitySeed
+	{
+		public  static async  Task SeedDataAsync(UserManager<ApplicationUser> userManager,
+			RoleManager<IdentityRole> roleManager, IConfiguration config)
+		{
+			List<string> roles = new List<string>
+			{
+				"admin",
+				"user",
+				"author"
+			};
+			
+			foreach (var role in roles)
+			{
+				if (await roleManager.RoleExistsAsync(role)!)
+				{
+					var t = roleManager.CreateAsync(new IdentityRole(role));
+					t.Wait();
+				}
+			}
+
+			string userName = config["SuperUser:name"];
+			string email = config["SuperUser:email"];
+			string password = config["SuperUser:password"];
+			
+			var user = new ApplicationUser
+			{
+				Email = email,
+				UserName = userName
+			};
+
+			if (await userManager.FindByEmailAsync(user.Email) is null)
+			{
+				var result = await userManager.CreateAsync(user, password);
+				if (result.Succeeded)
+				{
+					await userManager.AddToRoleAsync(user, "admin");
+				}
+			}
+		}
+	}
+}
--- Card_File.WEB/Program.cs
+++ Card_File.WEB/Program.cs
@@ -6,14 +6,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Card_File.BLL.Infrastructure;
+using Card_File.DAL.EF;
+using Card_File.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Card_File.WEB
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //init
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    var config = services.GetRequiredService<IConfiguration>();
+                    await IdentitySeed.SeedDataAsync(userManager, roleManager, config);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while seeding the identity");
+                }
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
